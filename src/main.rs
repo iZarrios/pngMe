58562c5345b679e8aa695d1e5988974ff434fd50
iZarrios <zarriose@gmail.com>
2024-09-27 12:29:10 +0300

    update: parse and use cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::str::FromStr;
+use std::process;
 
-use chunk_type::ChunkType;
+use args::{Cli, Commands};
+use clap::Parser;
+use commands::run;
 
 mod args;
 mod chunk;
@@ -12,31 +14,21 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    let str = "This is where your secret message will be!"
-        .as_bytes()
-        .to_vec();
+    let cli = Cli::parse();
 
-    let crc32 = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
-    let chunk_type = ChunkType::from_str("RuSt").unwrap();
-    let bytes: Vec<_> = chunk_type
-        .bytes()
-        .iter()
-        .chain(str.iter())
-        .copied()
-        .collect();
-    let crcc = crc32.checksum(&bytes);
-    println!("{crcc:?}");
-
-    println!("{:?}", list_files("."));
+    if let Err(error) = run(&cli) {
+        eprintln!("An error occurred: {}", error);
+        process::exit(1);
+    }
 
     Ok(())
 }
 
-fn list_files(path: &str) -> Result<Vec<String>> {
-    let files = std::fs::read_dir(path)?
-        .filter_map(|re| re.ok())
-        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
-        .filter_map(|e| e.file_name().into_string().ok())
-        .collect();
-    Ok(files)
-}
+// fn list_files(path: &str) -> Result<Vec<String>> {
+//     let files = std::fs::read_dir(path)?
+//         .filter_map(|re| re.ok())
+//         .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+//         .filter_map(|e| e.file_name().into_string().ok())
+//         .collect();
+//     Ok(files)
+// }
