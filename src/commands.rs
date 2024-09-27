aca5835d97686dcd71d4e54e3b46bb37bf72e618
iZarrios <zarriose@gmail.com>
2024-09-27 12:28:38 +0300

    add: subcommands using clap and derive feature

---
+++ src/commands.rs
@@ -0,0 +1,106 @@
+use std::{fs, path::PathBuf, str::FromStr};
+
+use crate::{
+    args::{Cli, Commands},
+    chunk::Chunk,
+    chunk_type::ChunkType,
+    png::Png,
+    Result,
+};
+
+pub fn run(args: &Cli) -> Result<()> {
+    match &args.command {
+        Commands::Encode {
+            png_file: file_path,
+            chunk_type,
+            message,
+        } => encode(file_path, chunk_type, message)?,
+
+        Commands::Decode {
+            png_file: file_path,
+            chunk_type,
+        } => decode(file_path, chunk_type)?,
+
+        Commands::Remove {
+            png_file: file_path,
+            chunk_type,
+        } => remove(&file_path, &chunk_type)?,
+
+        Commands::Print { png_file } => print(png_file)?,
+    }
+
+    Ok(())
+}
+
+fn encode(file_path: &PathBuf, chunk_type: &str, message: &str) -> Result<()> {
+    if file_path.extension().unwrap() != "png" {
+        return Err("This program takes only PNG files".into());
+    }
+
+    let file = fs::read(file_path)?;
+
+    let mut png = Png::try_from(file.as_slice())?;
+
+    let chunk_type = ChunkType::from_str(chunk_type)?;
+    let chunk = Chunk::new(chunk_type, message.as_bytes().to_vec());
+
+    png.append_chunk(chunk);
+
+    let _ = fs::write(file_path, png.as_bytes());
+
+    println!("Message encoded successfully!");
+
+    Ok(())
+}
+
+fn decode(file_path: &PathBuf, chunk_type: &str) -> Result<()> {
+    if file_path.extension().unwrap() != "png" {
+        return Err("This program takes only PNG files".into());
+    }
+
+    let file = fs::read(file_path)?;
+
+    let png = Png::try_from(file.as_slice())?;
+
+    match png.chunk_by_type(chunk_type) {
+        Some(chunk) => {
+            println!("Message: {:?}", chunk.data_as_string().unwrap());
+        }
+        None => println!("No message hidden in this image with this chunk type"),
+    }
+
+    Ok(())
+}
+
+fn remove(file_path: &PathBuf, chunk_type: &str) -> Result<()> {
+    if file_path.extension().unwrap() != "png" {
+        return Err("This program takes only PNG files".into());
+    }
+
+    let file = fs::read(file_path)?;
+
+    let mut png = Png::try_from(file.as_slice())?;
+
+    png.remove_first_chunk(chunk_type)?;
+
+    let _ = fs::write(file_path, png.as_bytes())?;
+
+    println!("Message has been removed successfully!");
+
+    Ok(())
+}
+
+fn print(file_path: &PathBuf) -> Result<()> {
+    if file_path.extension().unwrap() != "png" {
+        return Err("This program takes only PNG files".into());
+    }
+
+    let file = fs::read(file_path)?;
+
+    let png = Png::try_from(file.as_slice())?;
+
+    println!("{}", png);
+
+    Ok(())
+}
+
